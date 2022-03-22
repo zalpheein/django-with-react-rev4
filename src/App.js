66b457b9b5b8843b8469603c8e8b39347d3a8137
xalphee <zalphee@gmail.com>
2022-03-22 11:31:27 +0900

    setState 의 인자로 1)객체를 사용하는 방법과 2)함수를 사용하는 방법(선호)...

--- src/App.js
+++ src/App.js
@@ -14,7 +14,16 @@ class Counter1 extends React.Component {
 
         const value = this.state.value + 1;
         // 상태값을 설정시 setState 이용...상태값 변경 시.. 반드시 setState 함수 이용
+        // 하지만 setState 는 비동기로 동작... 그러므로 연속해서 특정 상태값을 변경 하려면...
+        // prevState 을 이용하여 순차적 완료 방식으로 코딩 해야함.
+        // this.setState( 오브젝트/객체  또는 함수, 콜백함수 ); 1개 인자 또는 콜백을 포함한 2개 인자 지정
+
+        // 객체일 경우
         this.setState( { value } );
+
+        // 함수일 경우... "바로 직전의 상태값" 인 prevState 가 인자로 반환됨... <---- 사용 권장 방식
+        // this.setState( (prevState) => { ~~ } );
+        // this.setState( prevState => { ~~ } );
     };
 
     render() {
